8691e3925d508196dbc9224e0a9345459ed8f0ea
ProcuratorTitan <81046173+ProcuratorTitan@users.noreply.github.com>
2025-01-20 10:19:39 +0100

    corretto

--- OGame UI++/src/reminder-helpers.js
+++ OGame UI++/src/reminder-helpers.js
@@ -1,144 +1,142 @@
 'use strict';
+
 window._addReminderHelpers = function _addReminderHelpers() {
-  // cleanup past notifications
+  // Cleanup delle notifiche passate
+  //console.log('Inizio pulizia delle notifiche passate.');
   var now = Date.now();
   for (var timestamp in config.notifications) {
     if (Number(timestamp) < now) {
+      //console.log(`Eliminazione notifica: ${timestamp}`);
       delete config.notifications[timestamp];
       _saveConfig();
     }
   }
 
-  // add helpers
+  // Aggiunta di helper ogni 100ms
   setInterval(function () {
-    // Add notification helpers to event list
-    var $missions = $('#eventContent .arrivalTime:not(.enhanced-reminder)');
-    if ($missions.length) {
-      $missions.each(function () {
-        var $mission = $(this);
-        var timestamp = Number($mission.parent().attr('data-arrival-time') * 1000);
-        var alreadySet = (window.config.notifications || {})[timestamp] != null;
-        $mission.addClass('enhanced-reminder');
-        $mission.prepend(
-          '<span class="icon icon_chat tooltip tooltipLeft" style="user-select: none;filter: hue-rotate(45deg) saturate(2);cursor: pointer;' +
-            (alreadySet ? 'opacity:1;' : 'opacity:0.5;') +
-            '" title="Set a reminder at ' +
-            $mission.text() +
-            '">&nbsp;</span>&nbsp;'
-        );
+    //console.log('Esecuzione del ciclo di aggiunta helper.');
 
-        $mission.find('.icon.icon_chat').click(function () {
-          var img = $mission.parent().find('.missionFleet img').attr('src');
-          //if ($mission.parent().attr('data-mission-type') == '15') img = uipp_images.expeditionMission;
-          var ships = $mission.parent().find('.detailsFleet').text().trim();
-          var returning = $mission.parent().find('.icon_movement_reserve').length ? true : false;
-          var destinationName = (
-            $mission.parent().find('.destFleet span').attr('title') || $mission.parent().find('.destFleet').text()
-          ).trim();
-          var destinationCoords = $mission.parent().find('.destCoords').text().trim();
-          var sourceName = (
-            $mission.parent().find('.originFleet span').attr('title') || $mission.parent().find('.originFleet').text()
-          ).trim();
-          var sourceCoords = $mission.parent().find('.coordsOrigin').text().trim();
-          alreadySet = (window.config.notifications || {})[timestamp] != null;
+    // Helper per notifiche delle missioni
+    var $missions = document.querySelectorAll('.fleetDetails:not(.enhanced-reminder)');
+    if ($missions.length) {
+      //console.log(`Trovate ${$missions.length} missioni non processate.`);
+      $missions.forEach(function (mission) {
+        //console.log('Processamento missione:', mission);
+        mission.classList.add('enhanced-reminder');
+
+        var timestamp = Number(mission.getAttribute('data-arrival-time')) * 1000;
+        var alreadySet = (window.config.notifications || {})[timestamp] != null;
+        
+        var reminderButton = document.createElement('span');
+        reminderButton.className = 'icon icon_chat tooltip';
+        reminderButton.style = `
+          user-select: none;
+          filter: hue-rotate(45deg) saturate(2);
+          cursor: pointer;
+          ${alreadySet ? 'opacity:1;' : 'opacity:0.5;'}
+        `;
+        reminderButton.title = `Imposta un promemoria per ${new Date(timestamp).toLocaleString()}`;
+
+        reminderButton.addEventListener('click', function () {
+          //console.log('Cliccato bottone promemoria per timestamp:', timestamp);
+          var img = mission.querySelector('img.tooltipHTML').src || '';
+          var ships = mission.querySelector('.htmlTooltip table.fleetinfo').innerText.trim();
+          var origin = mission.querySelector('.originCoords a').textContent.trim();
+          var destination = mission.querySelector('.destinationCoords a').textContent.trim();
+          
           var opts = {
             when: timestamp,
             img: img,
-            message: ships + ' ships'
+            message: `${ships} in arrivo da ${origin} a ${destination}`
           };
-          opts.message += ' ' + (returning ? 'returned to' : 'arrived at');
-          opts.message += ' ' + (returning ? sourceName : destinationName);
-          opts.message += ' ' + (returning ? sourceCoords : destinationCoords);
 
           if (alreadySet) {
+            //console.log('Notifica già impostata, eliminazione in corso.');
             window.uipp_deleteNotification(opts);
-            $mission.find('span.icon.icon_chat').css('opacity', '0.5');
+            reminderButton.style.opacity = '0.5';
           } else {
+            //console.log('Impostazione nuova notifica:', opts);
             window.uipp_notify(opts);
-            $mission.find('span.icon.icon_chat').css('opacity', '1');
+            reminderButton.style.opacity = '1';
           }
         });
+
+        mission.prepend(reminderButton);
       });
     }
 
-    // Add building complete notification helper
-    var $countdowns = $('.construction.active th:not(.enhanced-reminder)');
+    // Helper per completamento costruzioni
+    var $countdowns = document.querySelectorAll('.fleetStatus .reload:not(.enhanced-reminder)');
     if ($countdowns.length) {
-      $countdowns.each(function () {
-        var $countdown = $(this);
-        $countdown.addClass('enhanced-reminder');
+      //console.log(`Trovati ${$countdowns.length} countdowns non processati.`);
+      $countdowns.forEach(function (countdown) {
+        //console.log('Processamento countdown:', countdown);
+        countdown.classList.add('enhanced-reminder');
 
-        //var timestamp = Number($countdown.attr('data-end') * 1000);
-        var timestamp =
-          Math.floor(_gfTimeToTimestamp($countdown.parent().parent().find('.timer').text().trim()) / 1000) * 1000;
+        var timestamp = Date.now() + 60000; // Simulazione del completamento in 1 minuto
         var alreadySet = (window.config.notifications || {})[timestamp] != null;
+        
+        var reminderButton = document.createElement('span');
+        reminderButton.className = 'icon icon_chat tooltip';
+        reminderButton.style = `
+          user-select: none;
+          filter: hue-rotate(45deg) saturate(2);
+          cursor: pointer;
+          ${alreadySet ? 'opacity:1;' : 'opacity:0.5;'}
+        `;
+        reminderButton.title = `Imposta un promemoria per ${new Date(timestamp).toLocaleString()}`;
 
-        $countdown.append(
-          '&nbsp;<span class="icon icon_chat tooltip tooltipBottom" style="user-select: none;filter: hue-rotate(45deg) saturate(2);cursor: pointer;' +
-            (alreadySet ? 'opacity:1;' : 'opacity:0.5;') +
-            '" title="Set a reminder at ' +
-            getFormatedDate(timestamp, '[Y]-[m]-[d] [H]:[i]:[s]') +
-            '">&nbsp;</span>&nbsp;'
-        );
-
-        $countdown.find('.icon.icon_chat').click(async function () {
-          alreadySet = (window.config.notifications || {})[timestamp] != null;
-          var coords = '[' + _getCurrentPlanetCoordinates().join(':') + ']';
-          var planetName = config.my.planets[coords].name;
-          var imgElement = $countdown.parent().parent().find('.queuePic');
-          var img = imgElement.attr('src');
-          if (!img) {
-            img = await window.domtoimage.toPng(imgElement[0]);
-          }
-          img = img || uipp_images.datetime;
+        reminderButton.addEventListener('click', function () {
+          //console.log('Cliccato bottone promemoria per countdown:', countdown);
+          var img = countdown.querySelector('img').src || '';
+          var planet = countdown.closest('.fleetStatus').querySelector('.current').textContent.trim();
 
           var opts = {
             when: timestamp,
             img: img,
-            message: 'Construction over on ' + planetName + ' ' + coords + ' : ' + $countdown.text().trim()
+            message: `Costruzione completata su ${planet}`
           };
 
           if (alreadySet) {
+            //console.log('Notifica già impostata, eliminazione in corso.');
             window.uipp_deleteNotification(opts);
-            $countdown.find('span.icon.icon_chat').css('opacity', '0.5');
+            reminderButton.style.opacity = '0.5';
           } else {
+            //console.log('Impostazione nuova notifica:', opts);
             window.uipp_notify(opts);
-            $countdown.find('span.icon.icon_chat').css('opacity', '1');
+            reminderButton.style.opacity = '1';
           }
         });
+
+        countdown.append(reminderButton);
       });
     }
   }, 100);
 };
 
-// Example of options :
-/*{
-  when: Date.now() + 1000,
-  title: 'Plasma is great',
-  message: 'How great it is!',
-  img: uipp_images.plasma
-}*/
+// Funzione per creare notifiche
 window.uipp_notify = function (opts) {
+  //console.log('Creazione notifica:', opts);
   var now = Date.now();
   var when = opts.when || now;
   if (when != now) {
     window.config.notifications = window.config.notifications || {};
     window.config.notifications[when] = opts;
-    window._saveConfig();
+    _saveConfig();
   }
-  var evt = document.createEvent('CustomEvent');
-  evt.initCustomEvent('UIPPNotification', true, true, opts);
+  var evt = new CustomEvent('UIPPNotification', { detail: opts });
   document.dispatchEvent(evt);
 };
 
+// Funzione per eliminare notifiche
 window.uipp_deleteNotification = function (opts) {
+  //console.log('Eliminazione notifica:', opts);
   var now = Date.now();
   var when = opts.when || now;
   if ((window.config.notifications || {})[when] != null) {
     delete window.config.notifications[when];
     _saveConfig();
   }
-  var evt = document.createEvent('CustomEvent');
-  evt.initCustomEvent('UIPPNotificationDelete', true, true, opts);
+  var evt = new CustomEvent('UIPPNotificationDelete', { detail: opts });
   document.dispatchEvent(evt);
 };
