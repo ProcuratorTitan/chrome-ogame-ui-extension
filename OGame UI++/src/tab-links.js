b222a07c69d1fa684242b43d113c38eed40d3b5a
ProcuratorTitan <81046173+ProcuratorTitan@users.noreply.github.com>
2025-01-20 10:32:01 +0100

    corretto

--- OGame UI++/src/tab-links.js
+++ OGame UI++/src/tab-links.js
@@ -1,57 +1,83 @@
 'use strict';
 
-window._addLinkTabs = function _addLinkTabs() {
-  var universe = $('[name="ogame-universe"]').attr('content');
-  var serverLang = universe.split('-')[1].split('.')[0];
-  var serverNum = universe.split('-')[0].replace('s', '');
-  var playerName = $('[name="ogame-player-name"]').attr('content');
+$(document).ready(function() {
+  // Assicura che window.config esista
+  window.config = window.config || {};
   window.config.links = window.config.links || {
-    TrashSim: 'https://trashsim.universeview.be?utm_source=uipp&utm_medium=browser-extension&utm_campaign=default-link',
-    TradeCalc: 'http://proxyforgame.com/us/ogame/calc/trade.php'
+    BattleSim: 'https://battlesim.logserver.org/it',
+    TradeCalc: 'https://proxyforgame.com/it/ogame/calc/trade.php',
+    OTools: 'https://www.ghiroblu.com/o-tools/it/building_costs/'
   };
 
-  function _getLink(template) {
-    return template
-      .replace('{serverLang}', serverLang)
-      .replace('{serverNum}', serverNum)
-      .replace('{playerName}', playerName);
-  }
+  // Aggiunge link ai tab
+  window._addLinkTabs = function _addLinkTabs() {
+    // Controlla se gli elementi necessari sono presenti nella pagina
+    var universeElement = $('[name="ogame-universe"]');
+    var playerNameElement = $('[name="ogame-player-name"]');
+    
+    if (universeElement.length > 0 && playerNameElement.length > 0) {
+      var universe = universeElement.attr('content');
+      var serverLang = universe.split('-')[1].split('.')[0];
+      var serverNum = universe.split('-')[0].replace('s', '');
+      var playerName = playerNameElement.attr('content');
 
-  // add entries in menu
-  var i = 0;
-  for (var key in window.config.links) {
-    var $entry = $(
-      [
-        '<li style="margin-top:' + (i === 0 ? '10' : '0') + 'px;" class="customlink' + ++i + '">',
-        '<a class="menubutton" href="' +
-          _getLink(window.config.links[key]) +
-          '" ' +
-          (window.config.links[key].indexOf('://') !== -1 ? 'target="_blank"' : '') +
-          '>',
-        '<span class="textlabel" style="color:white">' + key + '</span>',
-        '</a>',
-        '</li>'
-      ].join('')
-    );
-    $('#menuTable').append($entry);
-  }
-};
+      // Sostituisce template con i valori reali
+      function _getLink(template) {
+        return template.replace('{serverLang}', serverLang)
+                       .replace('{serverNum}', serverNum)
+                       .replace('{playerName}', playerName);
+      }
 
-window._removeLink = function _removeLink(key, element) {
-  delete window.config.links[key];
-  window._saveConfig();
+      // Aggiunge le voci al menu
+      var i = 0;
+      for (var key in window.config.links) {
+        var $entry = $(
+          [
+            '<li style="margin-top:' + (i === 0 ? '10' : '0') + 'px;" class="customlink' + ++i + '">',
+            '<a class="menubutton" href="' +
+              _getLink(window.config.links[key]) +
+              '" ' +
+              (window.config.links[key].indexOf('://') !== -1 ? 'target="_blank"' : '') +
+              '>',
+            '<span class="textlabel" style="color:white">' + key + '</span>',
+            '</a>',
+            '</li>'
+          ].join('')
+        );
+        $('#menuTable').append($entry);
+      }
+    } else {
+      console.error("Required elements are missing from the page.");
+    }
+  };
 
-  var $el = $(element);
-  $el.parent().remove();
-};
+  // Rimuove un link
+  window._removeLink = function _removeLink(key, element) {
+    if (window.config.links && window.config.links[key]) {
+      delete window.config.links[key];
+      window._saveConfig();
+  
+      var $el = $(element);
+      $el.parent().remove();
+    } else {
+      console.error("Link not found or already removed.");
+    }
+  };
 
-window._addLink = function _addLink() {
-  var label = window.prompt('Link label ?');
-  var url = window.prompt('URL ?');
+  // Aggiunge un link
+  window._addLink = function _addLink() {
+    var label = window.prompt('Link label?');
+    var url = window.prompt('URL?');
 
-  if (label && url) {
-    window.config.links[label] = url;
-    window._saveConfig();
-    document.location.reload();
-  }
-};
+    if (label && url) {
+      window.config.links[label] = url;
+      window._saveConfig();
+      document.location.reload();
+    } else {
+      console.error("Invalid input for label or URL.");
+    }
+  };
+
+  // Chiama la funzione per aggiungere i tab
+  window._addLinkTabs();
+});
